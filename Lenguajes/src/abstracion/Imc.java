c2ea6446e307c14461757a6d85fbf155ef422864
T-102 <T-102@PC246>
2017-10-02 19:42:11 -0500

    2-09-17

---
+++ Lenguajes/src/abstracion/Imc.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstracion;
+
+/**
+ *
+ * @author T-102
+ */
+public class Imc {
+    Usuario u;
+    String resultado;
+    
+    public String calcular (){
+        float mivalor=u.getPeso()/(u.getAltura()*u.getAltura());
+        resultado="Tu imc es " + mivalor;
+        return resultado;
+        
+    }
+    
+}
---
+++ Lenguajes/src/abstracion/Usuario.java
@@ -0,0 +1,27 @@
+package abstracion;
+
+/**
+ *
+ * @author T-102
+ */
+public class Usuario {
+     private float altura;
+     private float peso;
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = valor;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+       this.peso = peso;
+    }
+}
+    
